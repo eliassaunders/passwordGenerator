11524aea1831ad6e787f945bd4f85b706a1b6983
Eliadad <eliasashtonsaunders@gmail.com>
2021-04-04 16:52:13 -0700

    trying object method

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,15 +9,20 @@ function writePassword() {
 
     passwordText.value = password;
     
-}
+} 
+var passwordChar = [];
 function generatePassword() {
+   
     var length = Number(prompt("How long would you like your password to be?"));
+
+    var lowercase = confirm("Do you want lowercase letters in your password?");
+    var lowercaseCharset = "abcdefghijklmnopqrstuvwxyz";
+    var lowercaseLength = lowercaseCharset.length;
     var placeHold = "";
-    
-    var letters = confirm("Do you want letters in your password?");
-    var letterCharset = "abcdefghijklmnopqrstuvwxyz";
-    var letterLength = letterCharset.length;
-    
+
+    var uppercase = confirm("Do you want uppercase letters in your password?");
+    var uppercaseCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var uppercaseLength = uppercaseCharset.length;
 
     var numbers = confirm("Do you want numbers in your password?");
     var numbCharset = "123456789";
@@ -30,14 +35,13 @@ function generatePassword() {
     if (length < 8 || length > 128) {
             alert("The password must be between 8 and 128 characters.")
         }
-
-        for (var i = 0;  i < length; i++ ) {
-            if (letters &&! numbers &&! specChar) {
-            placeHold += letterCharset.charAt(Math.floor(Math.random() * letterLength));
-        } 
-        return placeHold;
-    } 
-}
-
+        //ask about making the charsets into an array that can be refrenced as one object for .charAt
+    if (lowercase &&! uppercase &&! numbers &&! specChar) {
+        for (var i = 0, n = lowercaseCharset.length; i < length; i++) {
+            placeHold += lowercaseCharset.charAt(Math.floor(Math.random() * n));
+        } return placeHold;
+    }
+};
+console.log(passwordChar);
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
