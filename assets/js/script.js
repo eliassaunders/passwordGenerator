0bb5d9d6b18329a21dd77e48b030e29b391a0281
Eliadad <eliasashtonsaunders@gmail.com>
2021-04-04 17:18:43 -0700

    creating combination library

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var charsets = {
+    l:"abcdefghijklmnopqrstuvwxyz",
+    u:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    n: "123456789",
+    s: "!@#$%^&*()"};
 
 // Write password to the #password input
 function writePassword() {
@@ -13,32 +18,30 @@ function writePassword() {
 var passwordChar = [];
 function generatePassword() {
    
+    var lu = charsets.l + charsets.u;
+
     var length = Number(prompt("How long would you like your password to be?"));
 
     var lowercase = confirm("Do you want lowercase letters in your password?");
-    var lowercaseCharset = "abcdefghijklmnopqrstuvwxyz";
-    var lowercaseLength = lowercaseCharset.length;
     var placeHold = "";
 
     var uppercase = confirm("Do you want uppercase letters in your password?");
-    var uppercaseCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var uppercaseLength = uppercaseCharset.length;
 
     var numbers = confirm("Do you want numbers in your password?");
-    var numbCharset = "123456789";
-    var numbLength = numbCharset.length;
 
     var specChar = confirm("Do you want special characters in your password?");
-    var specCharset = "/.,';][-=`:";
-    var specLength = specCharset.length;
     
     if (length < 8 || length > 128) {
             alert("The password must be between 8 and 128 characters.")
         }
         //ask about making the charsets into an array that can be refrenced as one object for .charAt
     if (lowercase &&! uppercase &&! numbers &&! specChar) {
-        for (var i = 0, n = lowercaseCharset.length; i < length; i++) {
-            placeHold += lowercaseCharset.charAt(Math.floor(Math.random() * n));
+        for (var i = 0, n = charsets.l.length; i < length; i++) {
+            placeHold += charsets.l.charAt(Math.floor(Math.random() * n));
+        } return placeHold;
+    } else if (lowercase && uppercase &&! numbers &&! specChar) {
+        for (var i = 0, n = lu.length; i < length; i++) {
+        placeHold += lu.charAt(Math.floor(Math.random() * n));
         } return placeHold;
     }
 };
